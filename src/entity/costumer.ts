aeac6ccd255d7cf42154632d7783d9d15a8b4a65
Antonio Fulgencio <antoniofulg@gmail.com>
2024-10-28 14:38:41 -0300

    Update entity methods

--- src/entity/costumer.ts
+++ src/entity/costumer.ts
@@ -2,30 +2,36 @@ class Costumer {
 	_id: string
 	_name: string
 	_address: string
+	_active: boolean = false
 
 	constructor(id: string, name: string, address: string) {
 		this._id = id
 		this._name = name
 		this._address = address
+		this.validate()
 	}
 
-	get id(): string {
-		return this._id
+	validate() {
+		if (this._id.length === 0) {
+			throw new Error("Id is required")
+		}
+		if (this._name.length === 0) {
+			throw new Error("Name is required")
+		}
 	}
 
-	get name(): string {
-		return this._name
-	}
-
-	get address(): string {
-		return this._address
-	}
-
-	set name(name: string) {
+	changeName(name: string) {
 		this._name = name
 	}
 
-	set address(address: string) {
-		this._address = address
+	activate() {
+		if (this._address.length === 0) {
+			throw new Error("Address is mandatory to activate a costumer")
+		}
+		this._active = true
+	}
+
+	deactivate() {
+		this._active = false
 	}
 }
