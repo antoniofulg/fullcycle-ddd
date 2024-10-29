0fef55f9785e2aced5a441cb6d14929c188972b0
Antonio Fulgencio <antoniofulg@gmail.com>
2024-10-29 18:33:55 -0300

    Add product to item relationship

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -20,10 +20,17 @@ describe("Order unit test", () => {
 
 	it("should calculate total", () => {
 		const order = new Order("123", "123", [
-			new OrderItem("123", "Item 1", 10),
-			new OrderItem("123", "Item 2", 20),
-			new OrderItem("123", "Item 3", 30),
+			new OrderItem("123", "Item 1", "123", 10, 2),
+			new OrderItem("123", "Item 2", "123", 20, 2),
+			new OrderItem("123", "Item 3", "123", 30, 2),
 		])
-		expect(order.total()).toBe(60)
+		expect(order.total()).toBe(120)
+	})
+
+	it("should throw error if the item quantity is less than 1", () => {
+		expect(
+			() =>
+				new Order("123", "123", [new OrderItem("123", "Item 1", "123", 10, 0)])
+		).toThrow(new Error("Quantity must be greater than 0"))
 	})
 })
--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,15 +1,33 @@
 export default class OrderItem {
 	private _id: string
 	private _name: string
+	private _productId: string
 	private _price: number
+	private _quantity: number
 
-	constructor(id: string, name: string, price: number) {
+	constructor(
+		id: string,
+		name: string,
+		productId: string,
+		price: number,
+		quantity: number
+	) {
 		this._id = id
 		this._name = name
+		this._productId = productId
 		this._price = price
+		this._quantity = quantity
 	}
 
 	get price(): number {
 		return this._price
+	}
+
+	get quantity(): number {
+		return this._quantity
+	}
+
+	orderItemTotal(): number {
+		return this._price * this._quantity
 	}
 }
