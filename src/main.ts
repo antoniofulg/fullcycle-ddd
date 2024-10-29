d69572f5b79ae349dabc8bf4fcc4d763938ffeac
Antonio Fulgencio <antoniofulg@gmail.com>
2024-10-29 18:40:51 -0300

    Add type verification on tests

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const address = new Address("Main St", 123, "12345", "Anytown")
 costumer.address = address
 costumer.activate()
 
-const item1 = new OrderItem("1", "Item 1", 100)
-const item2 = new OrderItem("2", "Item 2", 200)
+const item1 = new OrderItem("1", "Item 1", "123", 100, 2)
+const item2 = new OrderItem("2", "Item 2", "123", 200, 2)
 
 const order = new Order("1", "123", [item1, item2])
